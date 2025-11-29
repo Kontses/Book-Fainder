4c58dba2a6d4c52b235b2a891a7fa7e6f8fec48f
kontses <libys97@gmail.com>
2025-11-29 14:10:00 +0200

    Night Theme Toggle details

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -36,17 +36,23 @@ export const ThemeToggle = () => {
 
     await transition.ready;
 
+    const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${endRadius}px at ${x}px ${y}px)`,
+    ];
+
+    const isDark = newTheme === "dark";
+
     document.documentElement.animate(
       {
-        clipPath: [
-          `circle(0px at ${x}px ${y}px)`,
-          `circle(${endRadius}px at ${x}px ${y}px)`,
-        ],
+        clipPath: isDark ? clipPath : [...clipPath].reverse(),
       },
       {
         duration: 500,
         easing: "ease-in-out",
-        pseudoElement: "::view-transition-new(root)",
+        pseudoElement: isDark
+          ? "::view-transition-new(root)"
+          : "::view-transition-old(root)",
       }
     );
   };
